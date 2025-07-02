15fa6208e176a601d344dc09e7fc0465ad499128
yerke <yerkezhanm@gmail.com>
2025-07-02 15:52:14 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,97 +1,152 @@
-express = require('express');
-app = express();
-const PORT = 3000;
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
+//Деректер қоры
+const pool= require('./db');
+const app = express();
+app.use(cors());
 app.use(express.json())
-app.use(cors())
-
-const books = [
-  { id: 1, title: 'Абай жолы', author: 'М. Әуезов', year: 1942 },
-  { id: 2, title: 'Менің атым Қожа', author: 'Б. Соқпақбаев', year: 1957 },
-  { id: 3, title: 'Алтын сақа', author: 'Ертегі', year: 1970 },
-  { id: 4, title: 'Батырлар жыры', author: 'Халық шығармашылығы', year: 1950 }
-];
+const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World its my first Backend application');
+  res.send('Hello World!');
 });
 
 
+// HW 1-books
+app.get('/books/:id', async (req, res) => {
+    const {id} = req.params;
+    try{
+        const result = await pool.query('SELECT * FROM books WHERE id=$1', [id]);
+        if(result.rows.length === 0){
+            return res.status(404).json({error: 'Book not found'});
+        }
+        res.json(result.rows[0]);
+    }catch(err){
+        return res.status(500).json({error: 'Сервер қатесі'});
+    }
+})
 
-app.post('/api/books', (req, res) => {
-    const {title, author, year} = req.body;
-    if (!title && !author && !year){
-       return res.status(401).json({error: 'Деректерді енгіз.'})
+app.get('/books', async (req, res) => {
+    const {sort} =req.query;
+    try{
+        const result = await pool.query(`SELECT * FROM books order by title ${sort}`);
+        res.json(result.rows); // get http://localhost:3000/books?sort=asc
+    }catch(err){
+        
+        return res.status(500).json({error: 'Сервер қатесі'});
+    }});
+
+   
+
+//users
+app.put('/users/:id', async (req, res) => {
+    const {id} = req.params;
+    const {username,password, age, city} = req.body;
+    if (!username || !password || !age || !city) {
+        return res.status(400).json({error: 'All fields are required'});
     }
-    const existingBook = books.find(book =>      
-        book.title.toLowerCase() === title.toLowerCase() && book.author.toLowerCase() === author.toLowerCase()
-    )
+    try{
+        const result = await pool.query('UPDATE users SET username=$1, password=$2, age=$3, city=$4 WHERE id=$5 RETURNING *', [username,password, age, city, id]);
+        if(result.rows.length === 0){
+            return res.status(404).json({error: 'User not found'});
+        }
+        res.json(result.rows[0]);
+    }catch(err){
+        return res.status(500).json({error: err.message});
+    }
+})
+
+app.delete('/users/:id', async (req, res) => {
+    const {id} = req.params;
+    try{
+        const result = await pool.query('DELETE FROM users WHERE id=$1 RETURNING *', [id]);
+        if(result.rows.length === 0){
+            return res.status(404).json({error: 'User not found'});
+        }
+        res.json(result.rows[0]);
+    }catch(err){
+        return res.status(500).json({error: 'Сервер қатесі'});
+    }
+})
+
+//register
+app.post('/register', async (req, res) => {
+    const {username, password, age, city} = req.body;
+    console.log(req.body);
+    if (!username || !password || !age || !city) {
+        return res.status(400).json({error: 'All fields are required'});
+    }
+    try{
+        const existingUser = await pool.query('SELECT * FROM users WHERE username=$1', [username]);
+        if(existingUser.rows.length > 0){
+            return res.status(400).json({error: 'Username already exists'});
+        }
+        
+        const result = await pool.query('INSERT INTO users (username, password, age, city) VALUES ($1, $2, $3, $4) RETURNING *', 
+            [username, password, age, city]);
+            
+        res.status(201).json({message: 'User created successfully', user: result.rows[0]});
+    }catch(err){
+        return res.status(500).json({error: 'Сервер қатесі'});
+    }
+})
+
+//login
+app.post('/login', async (req, res) => {
+    const {username, password} = req.body;
+    if (!username || !password) {
+        return res.status(400).json({error: 'All fields are required'});
+    }
+    try{
+        const result = await pool.query('SELECT * FROM users WHERE username=$1 AND password=$2', [username, password]);
+        const user =result.rows[0]
+
+        if(!user){
+            return res.status(404).json({error: 'User not found'});
+        }
+        res.status(200).json({message: 'Login successful', user});
+    }catch(err){
     
-    if (existingBook) {
-        return res.status(409).json({error: 'this book is exist'})
+        return res.status(500).json({error: 'Сервер қатесі'});
     }
-    const newBook = {
-        id: books.length + 1, 
-        title, 
-        author, 
-        year
-    };
-    books.push(newBook)
-res.status(201).json({message: 'Кітап қосылды', book: newBook})
 })
 
 
+app.get('/users', async (req, res) => {
+    const {sort} =req.query;
+    try{
+        const result = await pool.query(`SELECT * FROM users order by name ${sort}`);
+        res.json(result.rows); // get http://localhost:3000/users?sort=asc
+    }catch(err){
+        
+        return res.status(500).json({error: 'Сервер қатесі'});
+    }});
+
+   //get  http://localhost:3000/users/search?name=han  //han included in name
+        app.get('/users/search', async (req, res) => {
+            const {name} = req.query;
+            try{
+                const result = await pool.query(`SELECT * FROM users WHERE Lower(name) LIKE Lower('%${name}%')`);
+                res.json(result.rows);
+            }catch(err){
+                return res.status(500).json({error: 'Сервер қатесі'});
+            }});
+
+ app.get('/users/:id', async (req, res) => {
+        const {id} = req.params;
+        try{
+            const result = await pool.query(`SELECT * FROM users WHERE id=${id}`);
+            if(result.rows.length === 0){
+                return res.status(404).json({error: 'User not found'});
+            }
+            res.json(result.rows[0]);
+        }catch(err){
+            return res.status(500).json({error: 'Сервер қатесі'});
+        }});
 
 
 
 
-
-
-
-
-
-
-
-// // app.get('/api/books', (req, res) => {
-// //     res.json(books);
-// // });
-
-// app.get('/api/books/search', (req, res) => {
-//     const {title} = req.query
-//     if(!title){
-//         res.json(books)
-//     }
-
-//     const filteredBooks = books.filter(book => book.title.toLowerCase().includes(title.toLowerCase()))
-    
-//     res.json(filteredBooks)
-// })
-
-// app.get('/api/books/:id', (req, res) => {
-//     const {id} = req.params
-//     const book = books.find(book => book.id === parseInt(id))
-//     if(!book){
-//         res.status(404).json ({error: 'Book not found'})
-//     }
-//     res.json(book)
-// })
-
-// app.get('/api/books', (req, res) => {
-//     const {sort} = req.query
-
-//     let result =[...books]
-//     if(sort === 'asc'){
-//         result.sort((a,b) => a.year - b.year)
-//     }else if(sort === 'desc'){
-//         result.sort((a,b) => b.year - a.year)
-//     }
-//     res.send(result)
-// })
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
-
-
-
-
